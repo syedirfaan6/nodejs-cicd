a3ab1e14e84a2591130018df1b6f7247c2104757
suryatinku <46423416+suryatinku@users.noreply.github.com>
2023-01-27 15:33:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('CICD Demo by Surya')
 })
 
 app.get('/status', (req, res) => {
